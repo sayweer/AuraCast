4fc3c8084a43f23e831c8cdc337a74031ff92a7f
sayweer <degirmenseyit@gmail.com>
2026-05-10 01:39:09 +0300

    01:38 phantomwalletAdapterfix

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -6,7 +6,6 @@ import {
   WalletProvider
 } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
 import '@solana/wallet-adapter-react-ui/styles.css'
 
@@ -16,10 +15,7 @@ export const SolanaWalletProvider: FC<Props> = ({ children }) => {
   const endpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL
     ?? clusterApiUrl('devnet')
 
-  const wallets = useMemo(
-    () => [new PhantomWalletAdapter()],
-    []
-  )
+  const wallets = useMemo(() => [], [])
 
   return (
     <ConnectionProvider endpoint={endpoint}>
