e9554843cbeed9e40600ef0ce1d8601f1f03347d
sayweer <degirmenseyit@gmail.com>
2026-05-20 13:00:17 +0300

    try blinks fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/actions.json",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, x-action-version, x-blockchain-ids" },
+        ],
+      },
+      {
+        source: "/api/actions/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, Content-Encoding, Accept-Encoding, x-accept-action-version, x-action-version, x-blockchain-ids" },
+          { key: "Access-Control-Expose-Headers", value: "x-action-version, x-blockchain-ids" }
+        ],
+      }
+    ];
+  },
+};
 
 export default nextConfig;
