b9a7b1751d3aaea8c6d258a2339bf6d4cf0603fc
sayweer <degirmenseyit@gmail.com>
2026-05-21 23:17:21 +0300

    pino-pretty fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'pino-pretty': false,
+    }
+    return config
+  },
   async headers() {
     return [
       {
